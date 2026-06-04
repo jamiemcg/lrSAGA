e14063a767a73b8f442623dac60956f6e96f51b2
Jamie McGowan <jamiemcgowan.dev@gmail.com>
2026-06-04 16:45:05 +0100

    move utils for conda build

--- conda_build_package.sh
+++ conda_build_package.sh
@@ -9,3 +9,8 @@ chmod +x $PREFIX/bin/gfa_stats.py
 chmod +x $PREFIX/bin/gfa_to_fasta.py
 chmod +x $PREFIX/bin/lrSAGA.py
 chmod +x $PREFIX/bin/preprocess_mbg_gfa.py
+
+# lrSAGA utils
+mkdir -p $SP_DIR/utils
+cp $SRC_DIR/utils/__init__.py $SP_DIR/utils/
+cp $SRC_DIR/utils/*.py $SP_DIR/utils/
